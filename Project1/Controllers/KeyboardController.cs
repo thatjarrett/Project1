006125ed3a13d65a84f3300d15040098a9b011ef
thatjarrett <thatjarrett@gmail.com>
2025-02-05 15:09:19 -0500

    cleaned up

--- Project1/Controllers/KeyboardController.cs
+++ Project1/Controllers/KeyboardController.cs
@@ -8,6 +8,7 @@ namespace Project1.Controllers
     internal class KeyboardController : IController
     {
         private readonly Dictionary<Keys, ICommand> _commands;
+        private readonly HashSet<Keys> _movementKeys;
         private ICommand _idleCommand;
         private KeyboardState _previousState;
 
@@ -16,6 +17,7 @@ namespace Project1.Controllers
             _commands = commands;
             _idleCommand = idleCommand;
             _previousState = Keyboard.GetState();
+            _movementKeys = new HashSet<Keys> { Keys.W, Keys.A, Keys.S, Keys.D, Keys.Up, Keys.Left, Keys.Down, Keys.Right };
         }
 
         public void Update(GameTime gameTime)
@@ -28,29 +30,31 @@ namespace Project1.Controllers
                 if (state.IsKeyDown(key))
                 {
                     _commands[key].Execute();
-                    if (key == Keys.W || key == Keys.A || key == Keys.S || key == Keys.D ||
-                        key == Keys.Up || key == Keys.Left || key == Keys.Down || key == Keys.Right)
+                    if (_movementKeys.Contains(key))
                     {
                         movementKeyPressed = true;
                     }
                 }
             }
 
-           
             if (!movementKeyPressed && WasMovementKeyPressed())
             {
                 _idleCommand.Execute();
             }
 
-            _previousState = state; 
+            _previousState = state;
         }
 
         private bool WasMovementKeyPressed()
         {
-            return _previousState.IsKeyDown(Keys.W) || _previousState.IsKeyDown(Keys.A) ||
-                   _previousState.IsKeyDown(Keys.S) || _previousState.IsKeyDown(Keys.D) ||
-                   _previousState.IsKeyDown(Keys.Up) || _previousState.IsKeyDown(Keys.Left) ||
-                   _previousState.IsKeyDown(Keys.Down) || _previousState.IsKeyDown(Keys.Right);
+            foreach (var key in _movementKeys)
+            {
+                if (_previousState.IsKeyDown(key))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
