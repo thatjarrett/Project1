08a69b379138215db6bfe5762f6c5c6aa9cfacb4
Nick <sanchez.1227@buckeyemail.osu.edu>
2025-02-10 16:10:01 -0500

    Finished walking state for aquamentus.

--- Project1/Entities/AquamentusWalkState.cs
+++ Project1/Entities/AquamentusWalkState.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Project1.Interfaces;
 
@@ -6,15 +7,22 @@ namespace Project1.Entities
 {
     public class AquamentusWalkState : IEnemyState
     {
-        private Direction _facingDirection = Direction.Left;
+        private Direction movingDirection = Direction.Left;
+        private double oscillationDuration;
+        Random random = new Random();
+
+        public AquamentusWalkState()
+        {
+            this.oscillationDuration = 1.0;
+        }
         public void Enter(IEnemy aquamentus)
         {
-            aquamentus.SetAnimation("MoveLeft");
+            aquamentus.SetAnimation("Walk");
         }
 
         public void MoveLeft(IEnemy aquamentus)
         {
-           
+           //N/A
         }
         public void MoveRight(IEnemy aquamentus)
         {
@@ -30,15 +38,34 @@ namespace Project1.Entities
         }
         public void Attack(IEnemy aquamentus)
         {
-            //aquamentus.ChangeState(new LinkAttackState(_facingDirection));
+            //aquamentus.ChangeState(new aquamentusAttackState());
         }
         public void Damage(IEnemy aquamentus)
         {
-            //aquamentus.ChangeState(new LinkDamageState(_facingDirection));
+            //aquamentus.ChangeState(new aquamentusDamageState());
         }
         public void Update(IEnemy aquamentus, GameTime gameTime)
         {
-            aquamentus.Move(-2, 0); // Move left
+            oscillationDuration -= gameTime.ElapsedGameTime.TotalSeconds;
+            if (oscillationDuration <= 0)
+            {
+                if(movingDirection == Direction.Left)
+                {
+                    movingDirection = Direction.Right;
+                } else
+                {
+                    movingDirection = Direction.Left;
+                }
+                oscillationDuration = random.NextDouble();
+            }
+            if (movingDirection == Direction.Left)
+            {
+                aquamentus.Move(-2, 0);
+            } else
+            {
+                aquamentus.Move(2, 0);
+            }
+            
         }
     }
 }
