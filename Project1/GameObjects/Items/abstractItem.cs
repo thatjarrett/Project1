19a2fc92662c9c7d455d82e60002c30ec69b0d16
Angstrom Sarkar <acsarkar@gmail.com>
2025-02-27 21:34:00 -0500

    added collision box stuff to the abstract item class

--- Project1/GameObjects/Items/abstractItem.cs
+++ Project1/GameObjects/Items/abstractItem.cs
@@ -3,26 +3,32 @@ using Microsoft.Xna.Framework.Graphics;
 using Project1.Interfaces;
 using Color = Microsoft.Xna.Framework.Color;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using Project1.Collision;
+
 
 
 namespace Project1.GameObjects.Items
 {
-	public abstract class abstractItem
-	{
+    public abstract class abstractItem
+    {
         ISprite sprite;
         Rectangle destinationRectangle;
         int frameState = 0;
         Vector2 pos;
 
+        private CollisionBox collider;
+
         public abstractItem(Texture2D texture)
         {
-           // this.texture = texture;
-            this.pos = new Vector2(200,300);
+            // this.texture = texture;
+            this.pos = new Vector2(200, 300);
+            collider = new CollisionBox((int)pos.X, (int)pos.Y);
+
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-        //    sprite.Draw(spriteBatch, _position, SpriteEffects.None);
+            //    sprite.Draw(spriteBatch, _position, SpriteEffects.None);
         }
 
         public void Update(GameTime gameTime)
@@ -41,6 +47,14 @@ namespace Project1.GameObjects.Items
         }
         public Vector2 getPosition() { return this.pos; }
         public void SetSprite(ISprite sprite) { this.sprite = sprite; }
-	}
+
+
+        public CollisionBox GetCollider()
+        {
+            return collider;
+        }
+
+
+    }
 }
 
