a0da457c0bda41002e6bca6abcc9dc6f25defbe0
Angstrom Sarkar <acsarkar@gmail.com>
2025-04-05 16:46:53 -0400

    updated enemy spawn animation

--- Project1/GameObjects/Animations/EnemySpawnCloud.cs
+++ Project1/GameObjects/Animations/EnemySpawnCloud.cs
@@ -1,27 +1,30 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Project1.Interfaces;
+using System;
 using Color = Microsoft.Xna.Framework.Color;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 
 namespace Project1.GameObjects.Animations
 {
-    public class EnemySpawnCloud : ISprite
+    public class EnemySpawnCloud : IAnimation
     {
         Texture2D texture;
         Rectangle destinationRectangle;
         int frameState = 0;
         Vector2 Pos;
 
+        bool active = true;
+
+        int timer = 0;
 
         public EnemySpawnCloud(Texture2D texture, Vector2 pos)
         {
             this.texture = texture;
             Pos = pos;
-
         }
-        public void Draw(SpriteBatch spriteBatch,Vector2 location, SpriteEffects spriteEffects)
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, SpriteEffects spriteEffects)
         {
 
             Rectangle[] sourceArray = new Rectangle[5];
@@ -37,9 +40,35 @@ namespace Project1.GameObjects.Animations
             spriteBatch.Draw(texture, destinationRectangle, sourceArray[frameState], Color.White);
         }
 
+        public void Draw(SpriteBatch spriteBatch, SpriteEffects spriteEffects)
+        {
+
+            Rectangle[] sourceArray = new Rectangle[5];
+            sourceArray[0] = new Rectangle(0, 0, 16, 16);
+            sourceArray[1] = new Rectangle(80, 0, 16, 16);
+            sourceArray[2] = new Rectangle(96, 0, 16, 16);
+            sourceArray[3] = new Rectangle(112, 0, 16, 16);
+            sourceArray[4] = new Rectangle(112, 0, 16, 16);
+            destinationRectangle = new Rectangle((int)Pos.X, (int)Pos.Y, 2 * 16, 2 * 16);
+
+
+
+            spriteBatch.Draw(texture, destinationRectangle, sourceArray[frameState], Color.White);
+        }
+
+        public bool isActive()
+        {
+            return active;
+        }
+
         public void Update(GameTime gameTime)
         {
-            frameState = gameTime.TotalGameTime.Milliseconds / 100 % 5;
+            frameState = gameTime.TotalGameTime.Milliseconds / 100 % 7;
+            timer++;
+            if (timer == 20)
+            {
+                active = false;
+            }
 
         }
         public void SetColor(Color _color)
