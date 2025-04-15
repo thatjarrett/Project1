f65ce6df0c0301dc2699a66e8a66563d0c999808
Angstrom Sarkar <acsarkar@gmail.com>
2025-04-15 17:51:17 -0400

    key skeleton does unique stuff now!

--- Project1/Entities/KeySkeleton.cs
+++ Project1/Entities/KeySkeleton.cs
@@ -2,12 +2,14 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Project1.Collision;
+using Project1.GameObjects.Items;
 using Project1.Interfaces;
 using Project1.LevelLoading;
 using Project1.Projectiles;
 using Project1.Sprites;
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Project1.Entities
 {
@@ -32,17 +34,21 @@ namespace Project1.Entities
         int damageFrameCounter = 0;
         bool hurting = false;
 
-        private IProjectile[] projectiles = null;
+        private IProjectile[] projectiles = new BoomerangProjectile[1];
 
-        private int health = 3;
+        private int health = 5;
 
         private bool alive = true;
 
         private LootTables lootTables = new LootTables();
+
+        private ISprite boomerang;
+        private Vector2 throwDirection;
+
         public KeySkeleton(Vector2 startPos)
         {
             position = startPos;
-            currentState = new SkeletonMoveState(Direction.Left, 1.0);
+            currentState = new KeySkeletonMoveState();
             collider = new CollisionBox((int)startPos.X, (int)startPos.Y);
         }
 
@@ -56,6 +62,22 @@ namespace Project1.Entities
             Rectangle[] skelRect = new Rectangle[] { new Rectangle(1, 59, 16, 16) };
 
             skeletonSprite = new NMoveAnim(enemyTexture, skelRect, 5);
+            skeletonSprite.SetColor(Color.Gold);
+
+            boomerang = new NMoveAnim(enemyTexture,
+                new Rectangle[]
+                {
+                    new Rectangle(290, 11, 8, 16),
+                    new Rectangle(299, 11, 8, 16),
+                    new Rectangle(308, 11, 8, 16)
+                }, 5);
+            //boomerangThrowable = new BoomerangProjectile(boomerang);
+            for (int x = 0; x < projectiles.Length; x++)
+            {
+                projectiles[x] = new BoomerangProjectile(position, boomerang);
+            }
+            //boomerangs[0] = new BoomerangProjectile(position, boomerang);
+            throwDirection = new Vector2(0, 1);
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -75,11 +97,16 @@ namespace Project1.Entities
             }
             else
             {
-                skeletonSprite.SetColor(Color.White);
+                skeletonSprite.SetColor(Color.Gold);
             }
             if ((damageFrameCounter / 5) % 2 == 0)
             {
                 skeletonSprite.Draw(spriteBatch, position, currentSpriteEffect);
+            }
+
+            foreach (var b in projectiles)
+            {
+                b.Draw(spriteBatch);
             }
         }
 
@@ -88,31 +115,42 @@ namespace Project1.Entities
             position.X += dx;
             position.Y += dy;
             collider.Move(dx, dy);
+            
         }
 
         public void MoveDown()
         {
-            //
+            throwDirection = new Vector2(0, 1);
+            Move(0, 2);
         }
 
         public void MoveLeft()
         {
-            //
+            throwDirection = new Vector2(-1, 0);
+            Move(-2, 0);
         }
 
         public void MoveRight()
         {
-            //
+            throwDirection = new Vector2(1, 0);
+            Move(2, 0);
         }
 
         public void MoveUp()
         {
-            //
+            throwDirection = new Vector2(0, -1);
+            Move(0, -2);
         }
 
         public void PerformAttack()
         {
-            //
+            foreach (var b in projectiles)
+            {
+                b.Throw(position, throwDirection);
+            }
+            //ChangeState(attacking);
+
+            //Task.Delay(1000).ContinueWith(_ => ChangeState(moving));
         }
 
         public void SetAnimation(string action)
@@ -158,6 +196,12 @@ namespace Project1.Entities
                 currentState.Update(this, gameTime);
 
                 skeletonSprite.Update(gameTime);
+
+                foreach (var b in projectiles)
+                {
+                    b.ownerPosition(position);
+                    b.Update(gameTime);
+                }
             }
         }
 
---
+++ Project1/Entities/KeySkeletonMoveState.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Project1.Interfaces;
+using System;
+using System.Diagnostics;
+
+
+namespace Project1.Entities
+{
+    public class KeySkeletonMoveState : IEnemyState
+    {
+        private Direction movingDirection = Direction.Down;
+        private double movementDuration = 0.25;
+        Random random = new Random();
+
+
+        public void Enter(IEnemy skeleton)
+        {
+            //N/A
+        }
+
+        public void MoveLeft(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void MoveRight(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void MoveUp(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void MoveDown(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void Attack(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void Damage(IEnemy skeleton)
+        {
+            //N/A
+        }
+        public void Update(IEnemy skeleton, GameTime gameTime)
+        {
+            double timeStep = gameTime.ElapsedGameTime.TotalSeconds;
+            movementDuration -= timeStep;
+
+            if (movementDuration <= 0)
+            {
+                int x = random.Next(5);
+
+                //Trace.WriteLine(x);
+
+                switch (x)
+                {
+                    case 0:
+                        movingDirection = Direction.Right;
+                        break;
+                    case 1:
+                        movingDirection = Direction.Down;
+                        break;
+                    case 2:
+                        movingDirection = Direction.Left;
+                        break;
+                    case 3:
+                        movingDirection = Direction.Up;
+                        break;
+                    case 4:
+                        movingDirection = Direction.None;
+                        movementDuration /= 4;
+                        break;
+                }
+
+                movementDuration = 1.0;
+            }
+
+            switch (movingDirection)
+            {
+                case Direction.Up:
+                    skeleton.MoveUp();
+                    break;
+                case Direction.Right:
+                    skeleton.MoveRight();
+                    break;
+                case Direction.Down:
+                    skeleton.MoveDown();
+                    break;
+                case Direction.Left:
+                    skeleton.MoveLeft();
+                    break;
+                case Direction.None:
+                    skeleton.PerformAttack();
+                    break;
+            }
+        }
+        public Direction GetDirection()
+        {
+            return movingDirection;
+        }
+
+        public double GetMovementDuration()
+        {
+            return movementDuration;
+        }
+    }
+}
