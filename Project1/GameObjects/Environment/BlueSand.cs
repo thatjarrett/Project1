461f56eb7d8e73a19631d5ba70d53be12502e94b
Angstrom Sarkar <acsarkar@gmail.com>
2025-04-16 17:44:51 -0400

    blue sand could not be walked on, for some reason. i fixed this

--- Project1/GameObjects/Environment/BlueSand.cs
+++ Project1/GameObjects/Environment/BlueSand.cs
@@ -5,7 +5,7 @@ namespace Project1.GameObjects.Environment
     public class BlueSand : environmentTile
     {
         public BlueSand(Vector2 pos) :
-            base(pos, true)
+            base(pos, false)
         { }
     }
 }
